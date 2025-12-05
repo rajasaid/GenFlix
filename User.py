f5d8b20ef178c60700f69999d091a0e181fc42fe
Raja Said <rs.behm@gmail.com>
2025-12-05 12:49:09 +0200

    added alex changes and did some fixes

--- User.py
+++ User.py
@@ -2,15 +2,15 @@
 from pprint import pprint 
 
 class User:
-    def __init__(self, id, name, age, preferences):
-        self.id = id
+    def __init__(self, user_id, name, age, preferences):
+        self.user_id = user_id
         self.name = name
         self.age = age
         self.preferences = preferences ## list of Genres that the user prefers to watch
         self.watch_history = []
     
     def __str__(self):
-        return f"User: {self.name}, ID: {self.id}"
+        return f"User: {self.name}, ID: {self.user_id}"
 
     def __repr__(self):
         return str(self)
--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 ## test and do the application of GenFlix
 from GenFlix import GenFlix
+from menu import initiate_genflix
 
 def main():
+    initiate_genflix()
     # Initialize GenFlix with desired numbers
-    genflix = GenFlix(number_of_users=10, number_of_movies=20, number_of_ratings=50)
-    
-    # Generate data
-    genflix.Start_Genflix()
-    
-    # Print generated data for verification
-    print("Generated Users:")
-    for user in genflix.users:
-        user.print_details()
-        print("-" * 40)
-    
-    print("\nGenerated Movies:")
-    for movie in genflix.movies:
-        print(movie)
-    
-    print("\nGenerated Ratings:")
-    for rating in genflix.ratings:
-        print(rating)
-    
-    movies = genflix.recommend_movies_to_user(1, 5)
-    print("\nRecommended Movies for User ID 1:")
-    for movie in movies:
-        print(movie)
-    # get recommendations similar to Firm serve as example
-    print(genflix.movies[0])
-    movies_similar_to_firm = genflix.recommend_similar_movies(genflix.movies[0].title, 5)
-    print("\nMovies similar to 'Firm serve':")
-    for movie in movies_similar_to_firm:
-        print(movie)
 if __name__ == "__main__":
     main()
